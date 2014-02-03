4c0cc03c8648cd1046a1032a1766b881cf463883
James Hurford <terrasea@gmail.com>
2014-02-03 13:03:55 +1300

    Store now stores addresses
    Created address model and am storing it using lawndart library.  This is done through StoreCtrl instance, as an Angular Service.  Started on the Resident model, which is also handled by StoreCtrl.

--- web/main.dart
+++ web/main.dart
@@ -2,12 +2,14 @@ import 'package:angular/angular.dart';
 import 'package:polymer/polymer.dart';
 //import 'package:angular/routing/module.dart';
 import 'package:angular_node_bind/angular_node_bind.dart';
-import 'package:gcanvas/store.dart';
+import 'package:gcanvas/gcanvas.dart';
 
 
 class StoreModule extends Module {
   StoreModule() {
     type(NodeBindDirective);
+    type(DatabaseCtrl);
+    type(AddressCtrl);
     type(RouteInitializer, implementedBy: StoreRouter);
   }
 }
@@ -16,4 +18,4 @@ main() {
   initPolymer().run((){
     ngBootstrap(module: new StoreModule());
   });
-}'
+}
