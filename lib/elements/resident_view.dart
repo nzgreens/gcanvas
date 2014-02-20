a260f2c8504301e13025755ce7c0e8ac47694872
James Hurford <terrasea@gmail.com>
2014-02-21 00:05:22 +1300

    Aletered resident-view to take css selected and deselected attributes

--- lib/elements/resident_view.dart
+++ lib/elements/resident_view.dart
@@ -5,6 +5,9 @@ import 'package:gcanvas/resident.dart';
 class ResidentViewElement extends PolymerElement {
   @published Resident resident;
 
+  @published String styleSelected = "solid 5px green";
+  @published String styleDeselected = "solid 1px blue";
+
   bool selected = false;
 
   ResidentViewElement.created() : super.created() {
@@ -17,12 +20,12 @@ class ResidentViewElement extends PolymerElement {
     if(!selected) {
       fire("resident-selected", detail: resident);
       //$['icon'].style.border = "solid 5px green";
-      style.border = "solid 5px green";
+      style.border = styleSelected;
       selected = true;
     } else {
       fire("resident-deselected", detail: resident);
       //$['icon'].style.border = "solid 1px blue";
-      style.border = "solid 1px blue";
+      style.border = styleDeselected;
       selected = false;
     }
   }
